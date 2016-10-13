0603ecc24db2b6d08c55f3da1db5a6c74d65a7eb
Dmitry Ratushnyy <dratushn@cisco.com>
2016-10-13 23:01:18 +0300

    Added tables to support test creation: iteration 1

--- roles/cloudpulse_db/files/cloudpulse_tables.sql
+++ roles/cloudpulse_db/files/cloudpulse_tables.sql
@@ -19,4 +19,24 @@ CREATE TABLE IF NOT EXISTS cpulselock (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS cloudpulse_tests (
+  id           INT NOT NULL AUTO_INCREMENT,
+  uuid       VARBINARY(128),
+  created_at   TIMESTAMP    DEFAULT current_timestamp,
+  updated_at   TIMESTAMP    DEFAULT 0,
+  type    VARCHAR(128),
+  name    VARCHAR(128),
+  endpoint VARCHAR(128),
+  commands     TEXT,
+  enabled     BOOLEAN,
 
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS service_tests (
+  id           INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  service      VARCHAR(128),
+  name         VARCHAR(128),
+  command      VARCHAR(128),
+  PRIMARY KEY (id)
+);
